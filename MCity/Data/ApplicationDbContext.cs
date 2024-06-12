5fd5133edb7d9d71c7032f927066a4b24b327bf6
rownox <rownox.ik@gmail.com>
2024-06-12 10:37:26 -0400

    Data Attempt

--- MCity/Data/ApplicationDbContext.cs
+++ MCity/Data/ApplicationDbContext.cs
@@ -1,7 +1,13 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Shared.Models;
 
 namespace MCity.Data {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) {
+        }
+
+        public DbSet<LearnPage> LearnPages { get; set; }
     }
 }
---
+++ Shared/Models/LearnPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models {
+    public class LearnPage {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Source { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastEdited { get; set; }
+        public string LastEditedBy { get; set; }
+    }
+}
