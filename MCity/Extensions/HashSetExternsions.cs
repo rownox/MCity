b32e9e422148ba57513ca92a63aeebcbc3113765
rownox <rownox.ik@gmail.com>
2024-07-08 11:27:37 -0400

    Optimized + readable links

---
+++ MCity/Extensions/HashSetExternsions.cs
@@ -0,0 +1,9 @@
+﻿namespace MCity.Extensions {
+    public static class HashSetExtensions {
+        public static void Toggle<T>(this HashSet<T> set, T item) {
+            if (!set.Remove(item)) {
+                set.Add(item);
+            }
+        }
+    }
+}
