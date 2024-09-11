800bc62ef54eff85dd752368e9ad73cb0f609997
rownox <rownox.ik@gmail.com>
2024-09-11 13:22:21 -0400

    h

--- MCity/Models/LearnTopic.cs
+++ MCity/Models/LearnTopic.cs
@@ -1,7 +1,7 @@
 ﻿namespace MCity.Models {
    public class LearnTopic {
       public int Id { get; set; }
-      public string? Title { get; set; }
+      public string Title { get; set; } = string.Empty;
       public List<LearnPage> Pages { get; set; } = new List<LearnPage>();
    }
 }
