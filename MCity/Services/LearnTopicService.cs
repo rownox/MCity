93d90e13fb7970bcc1383cecb9f1a957f212c35c
rownox <rownox.ik@gmail.com>
2024-10-17 12:02:12 -0400

    h

--- MCity/Services/LearnTopicService.cs
+++ MCity/Services/LearnTopicService.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MCity.Services {
    public class LearnTopicService : ILearnTopicService {
-
       private readonly ApplicationDbContext _context;
       private readonly ILogger<LearnTopicService> _logger;
 
@@ -15,29 +14,39 @@ namespace MCity.Services {
 
       public async Task<LearnTopic?> AddTopic(LearnTopic learnTopic) {
          using var transaction = await _context.Database.BeginTransactionAsync();
-
          LearnPage learnPage = new LearnPage() {
             LearnTopic = learnTopic,
             LearnTopicId = learnTopic.Id,
             Title = "Introduction"
          };
-
          _context.LearnTopics.Add(learnTopic);
          _context.LearnPages.Add(learnPage);
          await _context.SaveChangesAsync();
-
          learnTopic.Pages.Add(learnPage);
          await _context.SaveChangesAsync();
          await transaction.CommitAsync();
-
          return learnTopic;
       }
 
       public async Task<List<LearnTopic>> GetAllTopics() {
-         var learntopics = await _context.LearnTopics
-         .Include(t => t.Pages)
-         .ToListAsync();
-         return learntopics;
+         return await _context.LearnTopics
+             .Include(t => t.Pages)
+             .Include(t => t.SubTopics)
+             .ThenInclude(st => st.Pages)
+             .Include(t => t.SubTopics)
+             .ThenInclude(st => st.SubTopics)
+             .ToListAsync();
+      }
+
+      public async Task<List<LearnTopic>> GetTopLevelTopics() {
+         return await _context.LearnTopics
+             .Where(t => t.ParentTopicId == null)
+             .Include(t => t.Pages)
+             .Include(t => t.SubTopics)
+             .ThenInclude(st => st.Pages)
+             .Include(t => t.SubTopics)
+             .ThenInclude(st => st.SubTopics)
+             .ToListAsync();
       }
    }
 }
